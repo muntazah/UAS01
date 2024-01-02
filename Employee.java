b591601dd7c7a9c46383443a835729397c9de2dc
muntazah <149351419+muntazah@users.noreply.github.com>
2024-01-02 14:55:36 +0700

    Employee.java

--- Employee.java
+++ Employee.java
@@ -1,90 +1,81 @@
 /*
- * Klik nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt untuk mengganti lisensi
- * Klik nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java untuk mengedit template ini
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package uas_soal_no2;
+package main.employee;
 
 /**
  *
- * @author ASUS
+ * @author Axioo
  */
-    public class Employee {
-    private String name;
+class Employee {
 
-    public Employee(String name) {
-        this.name = name;
+    private String nama;
+
+    public Employee(String nama) {
+        this.nama = nama;
     }
 
-    public String getName() {
-        return name;
+    public String getNama() {
+        return nama;
     }
 
-    private void setName(String name) {
-        this.name = name;
+    public void setNama(String nama) {
+        this.nama = nama;
     }
 
     public void work() {
-        System.out.println("Employee is working.");
+        System.out.println("Employee is working");
     }
 }
 
-// Manager class
 class Manager extends Employee {
+
     public Manager(String name) {
         super(name);
     }
 
     @Override
     public void work() {
-        System.out.println("Manager is managing tasks.");
+        System.out.println(this.getNama()+" bekerja sebagai Manager");
     }
 }
 
-// Developer class
 class Developer extends Employee {
+
     public Developer(String name) {
         super(name);
     }
 
     @Override
     public void work() {
-        System.out.println("Developer is coding.");
+        System.out.println(this.getNama()+ " Mengembangkan sebuah aplikasi");
     }
 }
 
-// Designer class
 class Designer extends Employee {
+
     public Designer(String name) {
         super(name);
     }
 
     @Override
     public void work() {
-        System.out.println("Designer is designing.");
+        System.out.println(this.getNama() + " Membuat desain web");
     }
 }
 
-// Main class
-class Main {
+public class MainEmployee {
+
     public static void main(String[] args) {
-        // Create instances of Manager, Developer, and Designer
-        System.out.println("===== Manager =====");
-        Manager manager = new Manager("AFFA MUNTAZAH");
-        System.out.println("Manager Name: " + manager.getName());
-        manager.work(); // Memanggil metode work pada manager
-        
-        System.out.println();
-        
-        System.out.println("===== Developer =====");
-        Developer developer = new Developer("AYU");
-        System.out.println("Developer Name: " + developer.getName());
-        developer.work(); // Memanggil metode work pada developer
-        
-        System.out.println();
-        
-        System.out.println("===== Designer =====");
-        Designer designer = new Designer("Tuti");
-        System.out.println("Designer Name: " + designer.getName());
+        Manager manager = new Manager("Affa");
+        Developer developer = new Developer("Afi");
+        Designer designer = new Designer("Afun");
+
+        // Memanggil metode work() pada setiap objek
+        manager.work();
+        developer.work();
         designer.work();
     }
+
 }
